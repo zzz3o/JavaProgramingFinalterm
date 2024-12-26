48f70c0994a578f30154788fc280abd15129e70e
zzz3o <zzzeow3@gmail.com>
2024-12-26 19:32:46 +0900

    둥근 모서리 설정 클래스 수정

--- src/RoundedPanel.java
+++ src/RoundedPanel.java
@@ -18,4 +18,27 @@ public class RoundedPanel extends JPanel {
         g2.fillRoundRect(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius);
         g2.dispose();
     }
+
+    public static JButton createRoundedButton(String text, int cornerRadius, Color backgroundColor, Color foregroundColor) {
+        JButton button = new JButton(text) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.setColor(backgroundColor);
+                g2.fillRoundRect(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius);
+                super.paintComponent(g);
+                g2.dispose();
+            }
+
+            @Override
+            public void setContentAreaFilled(boolean b) {
+                // Do nothing to prevent background color from being reset
+            }
+        };
+        button.setOpaque(false);
+        button.setForeground(foregroundColor);
+        button.setBorderPainted(false);
+        return button;
+    }
 }
