4df881e25b0d324109c348d4ac08cd2ee07cd48f
zzz3o <zzzeow3@gmail.com>
2024-12-26 18:58:02 +0900

    둥근 모서리 설정 클래스 생성

---
+++ src/RoundedPanel.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RoundedPanel extends JPanel {
+    private int cornerRadius;
+
+    public RoundedPanel(int cornerRadius) {
+        this.cornerRadius = cornerRadius;
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius);
+        g2.dispose();
+    }
+}
